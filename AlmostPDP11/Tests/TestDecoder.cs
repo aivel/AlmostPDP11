d1228e572365c9802fa2a4c3d8d8df01c44d1035
Ivan Sharavuev <ivan-scharawuev@yandex.ru>
2016-11-06 16:54:25 +0300

    Add TestDecoder

--- AlmostPDP11/Tests/TestDecoder.cs
+++ AlmostPDP11/Tests/TestDecoder.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Decoder;
+using AlmostPDP11.VM.Decoder;
 using NUnit.Framework;
-using Decoder = Decoder.Decoder;
+using Decoder = AlmostPDP11.VM.Decoder.Decoder;
 
 namespace AlmostPDP11.Tests
 {
@@ -16,12 +16,12 @@ namespace AlmostPDP11.Tests
         public void Test_Something()
         {
             HashSet<Int16> set = new HashSet<short>(new short[]{1,2,3});
-            Assert.AreEqual(global::Decoder.Decoder.getMnemonic(36864), Mnemonic.MOVB);
-            Assert.AreEqual(global::Decoder.Decoder.getMnemonic(57344), Mnemonic.SUB);
-            Assert.AreEqual(global::Decoder.Decoder.getMnemonic(28699), Mnemonic.MUL);
-            Assert.AreEqual(global::Decoder.Decoder.getMnemonic(30720), Mnemonic.XOR);
-            Assert.AreEqual(global::Decoder.Decoder.getMnemonic(2624), Mnemonic.COM);
-            Assert.AreEqual(global::Decoder.Decoder.getMnemonic(1280), Mnemonic.BLT);
+            Assert.AreEqual(Decoder.GetMnemonic(36864), Mnemonic.MOVB);
+            Assert.AreEqual(Decoder.GetMnemonic(57344), Mnemonic.SUB);
+            Assert.AreEqual(Decoder.GetMnemonic(28699), Mnemonic.MUL);
+            Assert.AreEqual(Decoder.GetMnemonic(30720), Mnemonic.XOR);
+            Assert.AreEqual(Decoder.GetMnemonic(2624), Mnemonic.COM);
+            Assert.AreEqual(Decoder.GetMnemonic(1280), Mnemonic.BLT);
         }
     }
 }
