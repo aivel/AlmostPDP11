f6f960c399641aa928137b2c7d4dfc5eb71dd36c
Ivan Sharavuev <ivan-scharawuev@yandex.ru>
2016-11-14 16:35:06 +0300

    Fix Assembler.cs

--- AlmostPDP11/VM/Assembler/Assembler.cs
+++ AlmostPDP11/VM/Assembler/Assembler.cs
@@ -1,4 +1,24 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿using System;
+using System.Collections.Generic;
+
+namespace VM.Assembler
 {
-  public class $CLASS$ {$END$}
+    public class Assembler
+    {
+
+        public Assembler(IEnumerable<String> program, int baseAddress)
+        {
+            this.program = program;
+            BASEADDRESS = baseAddress;
+        }
+
+        public int BASEADDRESS { get; set; }
+        public IEnumerable<String> program { get; set; }
+        public IEnumerable<ushort> assambled { get; set; }
+
+        public void assamble()
+        {
+
+        }
+    }
 }
