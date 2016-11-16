df5e76b9e1e77a195348f9cfd2e9e12f6b775e26
Ivan Sharavuev <ivan-scharawuev@yandex.ru>
2016-11-16 12:20:13 +0300

    Script for translating to rgp byts

--- Resources/toBin.py
+++ Resources/toBin.py
@@ -6,16 +6,16 @@ def main(name):
     w,h = im.size
     vals = set()
     matrix = []
-    for x in range(w):
+    for x in range(h):
         col = []
-        for y in range(h):
-            value = 0 if pix[x,y] != (0,0,0,0) else 1
-            print(value,end='',sep='')
-            col.append(value)
-            vals.add(pix[x,y])
+        for y in range(w):
+            value = ['0' if v<128 else '1' for i,v in enumerate(pix[y,x]) if i<3]
+            digit = int('0b'+''.join(value),2)
+            print(digit,end='',sep=' ')
+            col.append(digit)
+            vals.add(digit)
         matrix.append(col)
-        print(vals)
-    
-
+        print()
+   
 if __name__=='__main__':
     main("joytic_cut-photo.ru")
